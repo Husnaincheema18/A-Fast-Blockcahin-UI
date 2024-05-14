c7d05fee1f1a7886befec61bb36ae270c989cd22
unknown <husnaincheema21@gmail.com>
2024-05-14 13:20:52 +0500

    Build file deleted

--- src/app/Components/Partners.jsx
+++ src/app/Components/Partners.jsx
@@ -5,7 +5,7 @@ const Partners = () => {
   return (
     <div className='w-[90%] lg:w-[100%]'>
         <p className='text-softblue text-lg uppercase font-inter text-center mb-5'>Powering tools and integrations from companies all around the world</p>
-        <div className='grid grid-cols-4 lg:grid-cols-7 lg:w-[63%] gap-9 place-items-center m-auto'>
+        <div className='grid grid-cols-4 lg:grid-cols-7 lg:w-[63%] w-[80%] gap-9 place-items-center m-auto'>
         {
             Icons.map((currentItem, index) => {
                 return <div key={index}><img src={currentItem.url} alt={currentItem.id} /></div>
